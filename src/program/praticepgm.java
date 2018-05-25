7c9399c00df2204ed2df22f0f672a1bb042633e4
arunpradeep3583 <arunpradeep3583@gmail.com>
2018-05-25 14:12:35 +0530

    add code

---
+++ src/program/praticepgm.java
@@ -0,0 +1,41 @@
+package program;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class praticepgm {
+	
+	public static void addlist() {
+		
+		
+		List<String> obj = new ArrayList<String>();
+		
+		obj.add("Arun");
+		obj.add("Pradeep");
+		System.out.println(obj);
+		
+		obj.add(0, "A");
+		System.out.println(obj);
+		
+		
+		obj.remove("A");
+		System.out.println(obj);
+		obj.remove(1);
+		System.out.println(obj);
+		obj.add("Pradeep");
+		obj.add("Pradeep");
+		System.out.println(obj);
+		obj.remove("pradeep");
+		System.out.println(obj);
+		obj.set(2, "pradeep A");
+		System.out.println(obj);
+		//int pos = obj.indexOf("Pradeep");
+		String ab = obj.get(0);
+		System.out.println("ab" + ab);
+	}
+	
+	public static void main(String[] args) {
+		addlist();
+	}
+
+}
